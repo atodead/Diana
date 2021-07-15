a636d0f6aa92f85ade58ccb14f6ea12ab9afd739
Aniketh Shet <74291292+atodead@users.noreply.github.com>
2021-07-15 11:07:31 +0530

    Now Edith has been renamed to diana

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import datetime
 import wikipedia
 import pyjokes
 import smtplib
+import requests, json
 listener = sr.Recognizer()
 vroom = pyttsx3.init()
 voices = vroom.getProperty('voices')
 server = smtplib.SMTP('smtp.gmail.com', 587)
+vroom.setProperty('voice', voices[1].id)
 
 
 def talk(text):
@@ -19,19 +21,20 @@ def talk(text):
 def take_command():
     try:
         with sr.Microphone() as source:
-            print('listening...')
+            talk('listening...')
             voice = listener.listen(source)
             command = listener.recognize_google(voice)
             command = command.lower()
-            if 'edith' in command:
-                command = command.replace('edith', '')
+            if 'diana' in command:
+                command = command.replace('diana', '')
+                talk('You told:'+command)
                 print(command)
     except:
-        pass
+        talk("sorry, i couldn't hear you. check your microphone and try again.")
     return command
 
 
-def run_alexa():
+def run_diana():
     command = take_command()
     print(command)
     if 'play' in command:
@@ -41,14 +44,14 @@ def run_alexa():
     elif 'time' in command:
         time = datetime.datetime.now().strftime('%I:%M %p')
         talk('Current time is ' + time)
-    elif 'who the heck is' in command:
+    elif 'tell me more about' in command:
         person = command.replace('who the is', '')
         info = wikipedia.summary(person, 1)
         print(info)
         talk(info)
     elif 'email' in command:
         email1 = str(input('Your email please:'))
-        password = str(input('Your password please'))
+        password = str(input('Your password please:'))
         recv = input('the person you want to send the email to:')
         content = str(input('The content of the message(Please write in one line only.):'))
         server.starttls()
@@ -58,11 +61,81 @@ def run_alexa():
             recv,
             content
         )
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
+    elif 'tell me a joke' in command:
+        gimmejokes = pyjokes.get_joke()
+        talk(gimmejokes)
+        print(gimmejokes)
+    elif 'who is your father' in command:
+        talk('The best programmer on earth, the smartest kid on earth, Aneeketh Shait.')
+    elif 'thank you' in command:
+        talk('Anything for you!')
+    elif 'good night' in command:
+        talk("Goodnight, sweet dreams and Oh! I almost forgot, don't let the bedbugs bite!")
+        print("Goodnight, sweet dreams and Oh! I almost forgot, don't let the bedbugs bite!")
+    elif 'who is better you or siri' in command:
+        talk('is that even a question? Of course it is me!')
+    elif 'who is better you or alexa' in command:
+        talk('is that even a question? Of course it is me!')
+    elif 'calculate' in command:
+        ask = str(input('the operation you want to conduct:'))
+        a = int(input('the first number:'))
+        b = int(input('the second number'))
+        if 'addition' in ask:
+            method = a + b
+            talk(method)
+            print('The answer is:', method)
+        if 'subtraction' in ask:
+            method = a - b
+            talk(method)
+            print('The answer is:', method)
+            print('The answer is:', method)
+        if 'multiplication' in ask:
+            method = a * b
+            talk(method)
+            print('The answer is:', method)
+        if 'division' in ask:
+            method = a / b
+            talk(method)
+            print('The answer is:', method)
+    elif 'weather' in command:
+        base__url = "https://api.openweathermap.org/data/2.5/weather?"
+        city = str(input('City you want to know the weather about(or nearest city also):'))
+        api_key = 'e4d1e3ddf4b5c49fd0060e30966e1904'
+        URL = base__url + "q=" + city + "&appid=" + api_key
+        response = requests.get(URL)
+        if response.status_code == 200:
+            data = response.json()
+            main = data['main']
+            temperature = main['temp']
+            humidity = main['humidity']
+            pressure = main['pressure']
+            report = data['weather']
+            talk(f"{city:-^30}")
+            print(f"{city:-^30}")
+            talk(f"Temperature: {temperature}")
+            print(f"Temperature: {temperature}")
+            talk(f"Humidity: {humidity}")
+            print(f"Humidity: {humidity}")
+            talk(f"Pressure: {pressure}")
+            print(f"Pressure: {pressure}")
+            talk(f"Weather Report: {report[0]['description']}")
+            print(f"Weather Report: {report[0]['description']}")
+        else:
+            print("Error in the HTTP request")
+    elif 'tell me what you can do' in command:
+        talk('i can do many things. I can tell you a joke, send an email, play songs, and tell you who is my father, and i will also tell you the weather to the nearest city if you wanted to!')
+        print('i can do many things. I can tell you a joke, send an email, play songs, and tell you who is my father, and i will also tell you the weather to the nearest city if you wanted to!')
+
+    elif "i don't know what i should do" in command:
+        talk('Go do what you like to have fun!')
+
+    elif 'tell me about yourself' in command:
+        talk("I am Diana, master Aneeketh's smart voice assistant. I'm named after princess diana from the amazons, inspired by the amazons of greek mythology. And, i love my name!")
+
+
     else:
-        talk('Please say the command again.')
+        talk('please repeat the command again')
 
 
 while True:
-    run_alexa()
+    run_diana()
